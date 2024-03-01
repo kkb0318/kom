c4e30fdcfccb8d0f5332c685f6f9ff9844644d57
kkb0318 <smallobuti@gmail.com>
2024-03-01 21:05:04 +0900

    add argocd scheme

--- cmd/main.go
+++ cmd/main.go
@@ -33,6 +33,7 @@ import (
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 
+	argov1alpha1 "github.com/kkb0318/argo-cd-api/api/v1alpha1"
 	komv1alpha1 "github.com/kkb0318/kom/api/v1alpha1"
 	"github.com/kkb0318/kom/internal/controller"
 	//+kubebuilder:scaffold:imports
@@ -47,6 +48,7 @@ func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
 	utilruntime.Must(komv1alpha1.AddToScheme(scheme))
+	utilruntime.Must(argov1alpha1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
